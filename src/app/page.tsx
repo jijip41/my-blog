6815fb479aea3f0c07fd2410d1c37b6dcb4ed9e3
jihye <jiji.pak41@gmail.com>
2025-02-10 02:08:56 +0000

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import FeaturedPosts from "./components/FeaturedPosts";
+
 export default function Home() {
   return (
-    <div>
-      <main></main>
-    </div>
+    <main className="p-4">
+      <section>
+        <h2 className="font-semibold text-gray-500">Featured Posts</h2>
+        <FeaturedPosts />
+      </section>
+      <section>
+        <h2 className="font-semibold text-gray-500">You may like</h2>
+      </section>
+    </main>
   );
 }
