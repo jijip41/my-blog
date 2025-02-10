2f2ce5b5bce513cb329448cef85bb326b3b85c47
jihye <jiji.pak41@gmail.com>
2025-02-10 02:35:35 +0000

    Update home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import FeaturedPosts from "./components/FeaturedPosts";
+import SuggestedPosts from "./components/SuggestedPosts";
 
 export default function Home() {
   return (
-    <main className="p-4">
-      <section>
-        <h2 className="font-semibold text-gray-500">Featured Posts</h2>
+    <main className="flex flex-col gap-y-8 p-4">
+      <section className="flex flex-col gap-y-4">
+        <h2 className="text-md font-semibold text-gray-500">Featured Posts</h2>
         <FeaturedPosts />
       </section>
-      <section>
-        <h2 className="font-semibold text-gray-500">You may like</h2>
+      <section className="flex flex-col gap-y-4">
+        <h2 className="text-md font-semibold text-gray-500">You may like</h2>
+        <SuggestedPosts />
       </section>
     </main>
   );
