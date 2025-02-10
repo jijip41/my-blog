f8666a1e1ce89cb5fa70b2a02eb04e3582e7504d
jihye <jiji.pak41@gmail.com>
2025-02-10 20:23:29 +0000

    Update posts layout with context provider and checkboxes

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import React from "react";
+import { CategoryCheckbox } from "../components/CategoryCheckbox";
+import { Category } from "../utils/getCategory";
+import { CategoryProvider } from "../context/categoryContext";
 
 export default function PostsLayout({
   children,
@@ -6,9 +11,21 @@ export default function PostsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section>
-      <div>{children}</div>
-      <h3>Category</h3>
-    </section>
+    <CategoryProvider>
+      <main className="flex justify-between p-4">
+        <section>
+          <div>{children}</div>
+        </section>
+        <section className="flex flex-col space-y-4 w-[20%] border-2 rounded-sm p-4">
+          <h2 className="text-md font-semibold text-gray-500">Category</h2>
+          <ul className="flex flex-col gap-y-2">
+            <CategoryCheckbox label={Category.Review} />
+            <CategoryCheckbox label={Category.NextJs} />
+            <CategoryCheckbox label={Category.React} />
+            <CategoryCheckbox label={Category.TailwindCss} />
+          </ul>
+        </section>
+      </main>
+    </CategoryProvider>
   );
 }
