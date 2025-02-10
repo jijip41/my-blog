6141e6f2f09f4a7ac7578434af5800a6943ef231
jihye <jiji.pak41@gmail.com>
2025-02-10 02:07:23 +0000

    Create Post component

---
+++ src/app/components/Post.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { PostProps } from "../services/getPosts";
+import Image from "next/image";
+import Chip from "./Chip";
+import getCategory from "../utils/getCategory";
+
+export default function Post({ post }: { post: PostProps }) {
+  const { title, description, date, category, path } = post;
+  return (
+    <article className="flex flex-col gap-y-2 border-2 shadow-sm rounded-t-md p-4">
+      <h3 className="font-semibold text-gray-500">{title}</h3>
+      <Image
+        src={`/images/${path}.png`}
+        alt={`image of ${description}`}
+        width={420}
+        height={340}
+        className="m-auto"
+      />
+      <p>✨ {description}</p>
+      <p className="text-sm ml-auto">{date}</p>
+      <Chip category={getCategory(category)} />
+    </article>
+  );
+}
