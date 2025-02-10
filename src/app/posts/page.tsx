d903eba30b35c34a7ad31dd8cb2b3ff3132892d8
jihye <jiji.pak41@gmail.com>
2025-02-10 20:24:09 +0000

    Update posts page to render posts based on selected categories

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
+import { CategoryContext } from "../context/categoryContext";
+import { getPostsByCategory } from "../services/getPostsByCategory";
 
 export default function postsPage() {
-  return <div>Posts</div>;
+  const { state } = useContext(CategoryContext);
+  const posts = getPostsByCategory(state);
+
+  return (
+    <div>
+      Posts
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
