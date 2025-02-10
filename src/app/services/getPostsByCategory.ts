ab2d17243c8757302e288bd2a7eb1acca44df379
jihye <jiji.pak41@gmail.com>
2025-02-10 16:54:34 +0000

    Add service function getPostsByCategory

---
+++ src/app/services/getPostsByCategory.ts
@@ -0,0 +1,12 @@
+import getCategory, { Category } from "../utils/getCategory";
+import jsonData from "../../../data/posts.json";
+
+export function getPostsByCategory(categories: Category[]) {
+  if (categories.length === 0) {
+    return jsonData;
+  }
+  return jsonData.filter((post) => {
+    const postCategory = getCategory(post.category);
+    return postCategory && categories.includes(postCategory);
+  });
+}
