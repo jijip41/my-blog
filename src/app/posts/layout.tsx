fa8592e22c75edb9da7d18ebf25703e65b0b5ed9
jihye <jiji.pak41@gmail.com>
2025-02-11 17:34:33 +0000

    Refactor: add li tag in ul

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -19,10 +19,18 @@ export default function PostsLayout({
         <section className="flex flex-col space-y-4 w-[20%] border-2 rounded-sm p-4">
           <h2 className="text-md font-semibold text-gray-500">Category</h2>
           <ul className="flex flex-col gap-y-2">
-            <CategoryCheckbox label={Category.Review} />
-            <CategoryCheckbox label={Category.NextJs} />
-            <CategoryCheckbox label={Category.React} />
-            <CategoryCheckbox label={Category.TailwindCss} />
+            <li>
+              <CategoryCheckbox label={Category.Review} />
+            </li>
+            <li>
+              <CategoryCheckbox label={Category.NextJs} />
+            </li>
+            <li>
+              <CategoryCheckbox label={Category.React} />
+            </li>
+            <li>
+              <CategoryCheckbox label={Category.TailwindCss} />
+            </li>
           </ul>
         </section>
       </main>
