c6030eb09c7902735bfdd261bccf6071701663a4
jihye <jiji.pak41@gmail.com>
2025-02-13 21:14:16 +0000

    Add link to navigate to the post

--- src/app/posts/components/Post.tsx
+++ src/app/posts/components/Post.tsx
@@ -2,27 +2,30 @@ import Chip from "@/app/components/Chip";
 import { PostProps } from "@/app/services/getPostsByFeature";
 import getCategory from "@/app/utils/getCategory";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Post({ post }: { post: PostProps }) {
-  const { title, description, date, category, path } = post;
+  const { title, description, date, category, path, id } = post;
 
   return (
-    <article className="flex gap-x-2 border rounded-md shadow-md">
-      <Image
-        src={`/images/${path}.png`}
-        alt={`image of ${title}`}
-        width={180}
-        height={80}
-        className="rounded-l-sm"
-      />
-      <div className="flex flex-col gap-y-2 p-4">
-        <h3 className="text-md font-semibold text-gray-500">{title}</h3>
-        <p>
-          {description} ({date})
-        </p>
-        <Chip category={getCategory(category)} />
-      </div>
-    </article>
+    <Link href={`/posts/${id}`}>
+      <article className="flex gap-x-2 border rounded-md shadow-md">
+        <Image
+          src={`/images/${path}.png`}
+          alt={`image of ${title}`}
+          width={180}
+          height={80}
+          className="rounded-l-sm"
+        />
+        <div className="flex flex-col gap-y-2 p-4">
+          <h3 className="text-md font-semibold text-gray-500">{title}</h3>
+          <p>
+            {description} ({date})
+          </p>
+          <Chip category={getCategory(category)} />
+        </div>
+      </article>
+    </Link>
   );
 }
