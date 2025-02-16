4a3406adcc7721f5df018f3930746007eb4edf87
jihye <jiji.pak41@gmail.com>
2025-02-16 17:38:51 +0000

    Move ui component out of route folder

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -3,11 +3,10 @@
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 
 const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
