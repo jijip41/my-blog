3cc35409754544bef9c2d871d867efcb5552afde
jihye <jiji.pak41@gmail.com>
2025-02-17 01:59:36 +0000

    Convert components to async

--- components/posts/FeaturedPosts.tsx
+++ components/posts/FeaturedPosts.tsx
@@ -3,7 +3,7 @@ import CardPost from "./CardPost";
 import { getPostsByFeature } from "@/lib/post";
 
 export default async function FeaturedPosts() {
-  const posts = getPostsByFeature(true);
+  const posts = await getPostsByFeature(true);
   return (
     <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {posts.map((post) => (
--- components/posts/SuggestedPosts.tsx
+++ components/posts/SuggestedPosts.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import CardPost from "./CardPost";
 import { getPostsByFeature } from "@/lib/post";
 
-export default function SuggestedPosts() {
-  const posts = getPostsByFeature(false);
+export default async function SuggestedPosts() {
+  const posts = await getPostsByFeature(false);
   return (
     <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {posts.map((post) => (
