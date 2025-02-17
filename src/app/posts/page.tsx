cb9735881fa5f0cd422e4bbe87bffb8ad20069eb
jihye <jiji.pak41@gmail.com>
2025-02-17 02:18:02 +0000

    Fix posts page to be server component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,20 +1,13 @@
-"use client";
+import { getAllPostData } from "@/lib/post";
+import PostsWithCategory from "@/components/posts/PostsWithCategory";
 
-import React, { useContext } from "react";
-import { CategoryContext } from "../context/categoryContext";
-import { getPostsByCategory } from "../services/getPostsByCategory";
-import InlinePost from "@/components/posts/InlinePost";
-
-export default function postsPage() {
-  const { state } = useContext(CategoryContext);
-  const posts = getPostsByCategory(state);
+export default async function postsPage() {
+  const posts = await getAllPostData();
 
   return (
     <div className="flex flex-col gap-y-2">
       Posts
-      {posts.map((post) => (
-        <InlinePost key={post.id} post={post} />
-      ))}
+      <PostsWithCategory posts={posts} />
     </div>
   );
 }
