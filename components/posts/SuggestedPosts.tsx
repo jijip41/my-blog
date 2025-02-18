3f3cc6e4ca0ca70f8ebe0c5833c65cab331b0e2a
jihye <jiji.pak41@gmail.com>
2025-02-18 01:00:12 +0000

    Create PostsCarousel component

--- components/posts/SuggestedPosts.tsx
+++ components/posts/SuggestedPosts.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import CardPost from "./CardPost";
 import { getPostsByFeature } from "@/lib/post";
+import PostsCarousel from "./Carousel";
 
 export default async function SuggestedPosts() {
   const posts = await getPostsByFeature(false);
   return (
-    <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {posts.map((post) => (
-        <CardPost post={post} key={post.id} />
-      ))}
+    <section className="">
+      <PostsCarousel posts={posts} />
     </section>
   );
 }
