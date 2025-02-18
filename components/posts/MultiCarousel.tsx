d90f231ad30043d1e4c927fc36d943b497287057
jihye <jiji.pak41@gmail.com>
2025-02-18 17:30:30 +0000

    Refactor multicarousel

---
+++ components/posts/MultiCarousel.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+export default function MultiCarousel({ children }: Props) {
+  return (
+    <div className="relative">
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlay
+        autoPlaySpeed={3000}
+        infinite
+        keyBoardControl
+        pauseOnHover
+        responsive={responsive}
+        shouldResetAutoplay
+        slidesToSlide={1}
+        swipeable
+        itemClass="m-2"
+      >
+        {children}
+      </Carousel>
+    </div>
+  );
+}
--- components/posts/SuggestedPosts.tsx
+++ components/posts/SuggestedPosts.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { getPostsByFeature } from "@/lib/post";
-import PostsCarousel from "./PostsCarousel";
+import MultiCarousel from "./MultiCarousel";
+import Image from "next/image";
+import Link from "next/link";
+import PostCard from "./PostCard";
+import PostGrid from "./PostGrid";
 
 export default async function SuggestedPosts() {
   const posts = await getPostsByFeature(false);
   return (
     <section className="flex flex-col space-y-4">
       <h2 className="text-lg font-semibold text-gray-500">You may like</h2>
-      <PostsCarousel posts={posts} />
+      <MultiCarousel>
+        {posts.map((post) => (
+          <PostCard post={post} key={post.id} />
+        ))}
+      </MultiCarousel>
     </section>
   );
 }
