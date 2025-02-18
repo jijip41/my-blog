accb29a5ba2614b814835214fa733f7c4fb861d8
jihye <jiji.pak41@gmail.com>
2025-02-18 01:49:40 +0000

    Add link to featured posts

--- components/posts/FeaturedPosts.tsx
+++ components/posts/FeaturedPosts.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import CardPost from "./CardPost";
 import { getPostsByFeature } from "@/lib/post";
+import Link from "next/link";
 
 export default async function FeaturedPosts() {
   const posts = await getPostsByFeature(true);
   return (
-    <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <section className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => (
-        <CardPost post={post} key={post.id} />
+        <Link href={`/posts/${post.id}`} key={post.id}>
+          <CardPost post={post} />
+        </Link>
       ))}
     </section>
   );
