14a95269dee002031735bef21e4fe12b0b82380a
jihye <jiji.pak41@gmail.com>
2025-02-18 17:30:54 +0000

    Refactor post components

--- components/posts/PostCard.tsx
+++ components/posts/PostCard.tsx
@@ -3,22 +3,25 @@ import Image from "next/image";
 import Chip from "../Chip";
 import getCategory from "@/lib/category";
 import { PostProps } from "@/lib/post";
+import Link from "next/link";
 
 export default function PostCard({ post }: { post: PostProps }) {
   const { title, description, date, category, path } = post;
   return (
-    <article className="flex flex-col gap-y-2 overflow-hidden rounded-t-md border p-4 shadow-sm">
-      <time className="self-end text-sm">{date}</time>
-      <h3 className="font-semibold text-gray-500">{title}</h3>
-      <Image
-        src={`/images/${path}.png`}
-        alt={`image of ${description}`}
-        width={420}
-        height={340}
-        className="m-auto"
-      />
-      <p className="w-full truncate">✨ {description}</p>
-      <Chip category={getCategory(category)} />
-    </article>
+    <Link href={`/posts/${post.id}`}>
+      <article className="flex flex-col gap-y-2 overflow-hidden rounded-t-md border p-4 shadow-sm hover:shadow-md">
+        <time className="self-end text-sm">{date}</time>
+        <h3 className="w-full truncate font-semibold text-gray-500">{title}</h3>
+        <Image
+          src={`/images/${path}.png`}
+          alt={`image of ${description}`}
+          width={360}
+          height={280}
+          className="m-auto"
+        />
+        <p className="w-full truncate">✨ {description}</p>
+        <Chip category={getCategory(category)} />
+      </article>
+    </Link>
   );
 }
--- components/posts/PostGrid.tsx
+++ components/posts/PostGrid.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PostCard from "./PostCard";
-import Link from "next/link";
 import { PostProps } from "@/lib/post";
 
 interface Props {
@@ -10,9 +9,7 @@ export default function PostGrid({ posts }: Props) {
   return (
     <div className="grid-col-1 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
       {posts.map((post) => (
-        <Link href={`/posts/${post.id}`} key={post.id}>
-          <PostCard post={post} />
-        </Link>
+        <PostCard post={post} key={post.id} />
       ))}
     </div>
   );
