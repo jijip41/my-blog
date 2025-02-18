a6ee3313d97febeab1bf39c969ff171d81d57ef4
jihye <jiji.pak41@gmail.com>
2025-02-18 13:11:05 +0000

    Create Hero component and import it in about page

---
+++ components/posts/Hero.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import SocialMedia from "../SocialMedia";
+import { getEnvironmentVariable } from "@/lib/configUtils";
+
+export default function Hero() {
+  return (
+    <>
+      <section className="flex flex-col items-center gap-y-4">
+        <Image
+          src={`${getEnvironmentVariable("PROFILE_PHOTO_URL")}`}
+          width={150}
+          height={150}
+          alt="profile photo"
+          className="rounded-full"
+        />
+        <SocialMedia />
+      </section>
+      <section className="flex flex-col items-center">
+        <p className="mb-2 font-semibold">{"Hi 👋 I'm Jihye."}</p>
+        <p>{"I'm a software engineer who loves to build things."}</p>
+        <p>
+          {
+            "I'm passionate about learning new technologies and sharing my knowledge with others."
+          }
+        </p>
+        <p>I hope I can share my journey with you through this blog.</p>
+      </section>
+    </>
+  );
+}
